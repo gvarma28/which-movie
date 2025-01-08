7a434642ac83ed8c58a3e1229776cc6ba62959e3
gvarma28 <gouravswork@gmail.com>
2025-01-08 19:05:31 +0530

    refactor: clean up

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 import axios from "axios";
 
-let config = {
-  method: "get",
-  maxBodyLength: Infinity,
-  url: "https://www.youtube.com/shorts/-BYMUKycQq8",
+const initialRequest = async (url) => {
+  let config = {
+    method: "GET",
+    maxBodyLength: Infinity,
+    url,
+  };
+  let response = await axios.request(config);
+  if (!response || response.status !== 200) {
+    throw new Error(`Invalid Response Received`, { status: response.status });
+  }
+  response = response.data.replaceAll("\n", "");
+  response = response.split("var ytInitialData = ")[1];
+  response = response.split(";</script>")[0];
+  response = JSON.parse(response);
+  return response;
 };
 
-let response = await axios.request(config);
-if (!response || response.status !== 200) {
-  throw new Error(`Invalid Response Received`, { status: response.status });
-}
-response = response.data.replaceAll("\n", "");
-response = response.split("var ytInitialData = ")[1];
-response = response.split(";</script>")[0];
-response = JSON.parse(response);
-
-let continuationCommand =
-  response.engagementPanels[0].engagementPanelSectionListRenderer.header
-    .engagementPanelTitleHeaderRenderer.menu.sortFilterSubMenuRenderer
-    .subMenuItems[0].serviceEndpoint.continuationCommand.token;
-
-let continuations;
-
-let res;
-let iteration = 0;
-while (true) {
-  iteration++;
-  if (iteration === 10 || !continuationCommand) break;
+const getComments = async (continuationCommand) => {
   let data = JSON.stringify({
     context: {
       client: {
@@ -36,7 +27,7 @@ while (true) {
     },
     continuation: continuationCommand,
   });
-  config = {
+  const config = {
     method: "post",
     maxBodyLength: Infinity,
     url: "https://www.youtube.com/youtubei/v1/browse?prettyPrint=false",
@@ -45,36 +36,70 @@ while (true) {
       "Content-Type": "application/json",
     },
   };
-
-  response = await axios.request(config);
+  const response = await axios.request(config);
   if (!response || response.status !== 200) {
     throw new Error(`Invalid Response Received`, { status: response.status });
   }
 
-  continuations =
+  const continuations =
     response?.data?.onResponseReceivedEndpoints[1]
       ?.reloadContinuationItemsCommand?.continuationItems ||
     response?.data?.onResponseReceivedEndpoints[0]
-      ?.appendContinuationItemsAction ?.continuationItems ||
+      ?.appendContinuationItemsAction?.continuationItems ||
     null;
 
   continuationCommand =
     continuations?.[20]?.continuationItemRenderer?.continuationEndpoint
       ?.continuationCommand?.token || null;
   // filter to have only comments elements
-  response = response.data.frameworkUpdates.entityBatchUpdate.mutations.filter(
-    (e) => e.payload.commentEntityPayload
-  );
+  const commentEntityPayload =
+    response.data.frameworkUpdates.entityBatchUpdate.mutations.filter(
+      (e) => e.payload.commentEntityPayload
+    );
   // further map to have only comments elements
-  let commentsObject = response.map(
+  const commentsObject = commentEntityPayload.map(
     (e) => e.payload.commentEntityPayload.properties
   );
 
-  let comments = commentsObject.map((e) => e.content.content);
+  const comments = commentsObject.mcap((e) => {
+    return { comment: e.content.content, comment_id: e.commentId };
+  });
 
+  return {
+    response,
+    comment_info: comments,
+    continuationCommand,
+  };
+};
 
-  res = commentsObject.filter((e) => e.content.content.includes("name"));
-  if (res && res.length > 0) break;
-}
+const main = async () => {
+  const response = await initialRequest(
+    "https://www.youtube.com/shorts/-BYMUKycQq8"
+  );
 
-console.log(res);
+  let continuationCommand =
+    response.engagementPanels[0].engagementPanelSectionListRenderer.header
+      .engagementPanelTitleHeaderRenderer.menu.sortFilterSubMenuRenderer
+      .subMenuItems[0].serviceEndpoint.continuationCommand.token;
+
+  let continuations;
+
+  let res;
+  let iteration = 0;
+  while (true) {
+    iteration++;
+    if (iteration === 10 || !continuationCommand) break;
+
+    const { comment_info, nextContinuationCommand } = await getComments(
+      continuationCommand
+    );
+    continuationCommand = nextContinuationCommand;
+
+    res = comment_info.filter((e) => e.comment.includes("name"));
+    if (res && res.length > 0) break;
+  }
+
+  console.log(res);
+};
+
+main();
