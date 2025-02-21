e43fa2465a7197bd2992b7e3b706beeb7149d9c6
gvarma28 <gouravswork@gmail.com>
2025-02-21 20:40:19 +0530

    feat: allowing cors which-movie.com

--- server/main.go
+++ server/main.go
@@ -89,7 +89,7 @@ func doMagic(w http.ResponseWriter, r *http.Request) {
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Add CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Allow your SvelteKit app
+		w.Header().Set("Access-Control-Allow-Origin", "https://which-movie.com") // Allow your SvelteKit app
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
